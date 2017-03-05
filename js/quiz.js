c06dbebf0d87b2504e2b3e75fc623f93d8a2c228
zearadoua <ad.spfoot@gmail.com>
2017-03-05 18:48:38 +0000

    Add files via upload

---
+++ js/quiz.js
@@ -0,0 +1,60 @@
+(function() {
+
+    var app = angular.module('myQuiz', []);
+
+    app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.activeQuestionAnswered = 0;
+        $scope.percentage = 0;
+
+        $http.get('quiz_data.json').then(function(quizData) {
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length;
+        });
+
+        $scope.selectAnswer = function(qIndex, aIndex) {
+            var questionState = $scope.myQuestions[qIndex].questionState;
+
+            if (questionState != 'answered') {
+                $scope.myQuestions[qIndex].selectedAnswer = aIndex;
+                var correctAnswer = $scope.myQuestions[qIndex].correct;
+                $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+
+                if (aIndex === correctAnswer) {
+                    $scope.myQuestions[qIndex].correctness = 'correct';
+                    $scope.score += 1;
+                } else {
+                    $scope.myQuestions[qIndex].correctness = 'incorrect';
+
+                }
+                $scope.myQuestions[qIndex].questionState = 'answered';
+            }
+            $scope.percentage = (($scope.score / $scope.totalQuestions) * 100).toFixed(1);
+
+        }
+
+        $scope.isSelected = function(qIndex, aIndex) {
+            return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+        }
+        $scope.isCorrect = function(qIndex, aIndex) {
+            return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+        }
+
+        $scope.selecContinue = function() {
+            return $scope.activeQuestion += 1;
+        }
+
+        $scope.createShareLinks = function(percentage) {
+            var url = 'http://zinradoua.me';
+            var emailLink = '<a class="btn email" href="mailto:?subject=Try to beat my score!&amp;body=I scoreda ' + percentage + '% o this quiz about Saturn. Try to beat my score at ' + url + '">Email a freind</a>';
+            var twitterLink = '<a class="btn twitter" target="_blank" href="http://twitter.com/share?text=I scored ' + percentage + '% in this quiz about Saturn. Try to beat my score at&amp;hashtags=SaturnQuiz&amp;url=' + url + '">Twitte your score</a>';
+            var newMarkup = emailLink + twitterLink;
+            return $sce.trustAsHtml(newMarkup);
+
+        }
+
+
+    }]);
+
+})();
